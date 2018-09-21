498ac0357e913b05e397b63fa6a639669591ccdb
Cameron Cooper <cameron.w.cooper@gmail.com>
2018-09-21 12:02:32 -0600

    init commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+var app = express();
+
+const PORT = process.env.PORT || 5000
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", function (req, res) {
+    res.render("index");
+});
+
+app.listen(PORT, function () {
+    console.log("Portfolio Launched on localhost:5000");
+});
